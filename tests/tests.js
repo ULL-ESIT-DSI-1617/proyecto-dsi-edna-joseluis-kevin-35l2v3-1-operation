eaf5b894091c311aa009963e1325e8e059a74876
alu0100821390 <alu0100821390@ull.edu.es>
2017-05-19 10:07:01 +0100

    Añadir Karma closes #4

--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,17 @@
-"use strict";
-
 var assert = chai.assert;
 
 suite('operation', function() {
+	setup(function() {
+		if (typeof __html__ !== 'undefined') {
+			document.body.innerHTML = __html__['tests/index.html'];
+			original = document.getElementById('original');
+			operated = document.getElementById('converted');
+		}
+	});
+	
 	test('2+3', function() {
-		original.value = '2+3';
+		original = '2+3';
 		main();
-		assert.match(operated.innerHTML, /ERROR/);
+		assert.match(operated, /ERROR/);
 	});
 });
