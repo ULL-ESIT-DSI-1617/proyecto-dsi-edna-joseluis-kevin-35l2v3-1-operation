76293c71b10adab97f53444ccb15d20d0c304632
alu0100821390 <alu0100821390@ull.edu.es>
2017-05-19 15:53:52 +0100

    Actualizar pruebas mocha chai

--- tests/tests.js
+++ tests/tests.js
@@ -5,12 +5,12 @@ suite('operation', function() {
 		if (typeof __html__ !== 'undefined') {
 			document.body.innerHTML = __html__['tests/index.html'];
 			original = document.getElementById('original');
-			operated = document.getElementById('converted');
+			operated = document.getElementById('operated');
 		}
 	});
 	
 	test('2+3', function() {
-		original = '2+3';
+		original.value = '2+3';
 		main();
 		assert.match(operated, /ERROR/);
 	});
